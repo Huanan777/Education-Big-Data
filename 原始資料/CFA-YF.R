c7ab04bda3a1f526a988c91b928c112794ca8027
411422259@m365.fju.edu.tw <411422259@m365.fju.edu.tw>
2025-06-08 20:11:02 +0800

    改題目組合

--- 原始資料/CFA-YF.R
+++ 原始資料/CFA-YF.R
@@ -12,12 +12,12 @@ data_items <- na.omit(data_items)
 
 # 建立六因子模型語法 F5 F3 F2 F4 F1 F6
 model <- '
-  F1 =~ v7 + v8 + v10 + v11 + v13
+  F1 =~ v2 + v3 + v5 + v8
   F2 =~ v14 + v16 + v17 + v18
   F3 =~ v21 + v22 + v23 + v24
   F4 =~ v28 + v29 + v30 + v31
-  F5 =~ v33 + v37 + v38 + v39 + v40 + v41
-  F6 =~ v46 + v47 + v50 +v52 + v54 + v57
+  F5 =~ v35 + v37 + v38 + v39 + v44
+  F6 =~ v47 + v50 + v52 + v57
 '
 
 # 執行確認性因素分析（CFA）
@@ -26,5 +26,4 @@ fit <- cfa(model, data = data_items, estimator = "MLR")
 
 # 顯示適配度指標（包含 CFI）
 fitMeasures(fit, c("cfi", "tli", "rmsea", "srmr"))
-
 
